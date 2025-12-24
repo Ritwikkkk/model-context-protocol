6b9b2aec4426ef847825afa3008f905fee678c46
Ritwik Ghosh <ritwikg82@gmail.com>
2025-12-24 22:21:20 +0530

    Adding new stock recommender code

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.agents import create_agent
 import warnings
 warnings.filterwarnings("ignore")
-
+import logging
+logging.getLogger("mcp").setLevel(logging.CRITICAL)
+logging.getLogger("langchain_mcp_adapters").setLevel(logging.CRITICAL)
+logging.getLogger("langchain.tools").setLevel(logging.CRITICAL)
+logging.disable(logging.CRITICAL)
 
 load_dotenv()
 
@@ -38,9 +42,17 @@ async def run_agent():
     system_prompt="You are a web search agent with access to brightdata tool to get latest data.",
     )
     agent_response = await agent.ainvoke({
-    "messages": [{"role": "user", "content": "How many runs did Virat Kohli score in today's Vijay Hazare Trophy match against AP?"}]
+    "messages": [{"role": "user", "content": "How many runs did Rohit Sharma score in today's Vijay Hazare Trophy match against AP?"}]
     })
-    print(agent_response["messages"][-1].content)
+    last_message = agent_response["messages"][-1]
+
+    # If the content is a list, extract the text from the first item
+    if isinstance(last_message.content, list):
+        text_output = last_message.content[0].get('text', '')
+    else:
+        text_output = last_message.content
+
+    print(text_output)
 
 if __name__ == "__main__":
     asyncio.run(run_agent())
